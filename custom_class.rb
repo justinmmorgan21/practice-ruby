386455db4451431a6a01edc73754876467476c99
justinmmorgan21 <justinmorgan21@gmail.com>
2024-08-27 18:44:23 -0500

    Add more work with lego class

--- custom_class.rb
+++ custom_class.rb
@@ -3,13 +3,18 @@
 # b) Create an instance from your store item class. Use puts statements to print the 3 attributes individually to the terminal.
 # c) Use the attr_writer method to change a value
 class LegoSet
+  attr_writer :price
+  attr_reader :design, :price, :difficulty
+
   def initialize(design, price, diff)
     @design = design
     @price = price
     @difficulty = diff
   end
 
-  
+  def to_s
+    "#{@design} lego set, on sale for $#{@price}, has a #{@difficulty} difficulty rating"
+  end
 
 end
 
@@ -17,4 +22,11 @@ lego_set1 = LegoSet.new("truck", 30, "medium")
 lego_set2 = LegoSet.new("skyscraper", 50, "hard")
 lego_set3 = LegoSet.new("jet ski", 20, "easy")
 lego_sets = [lego_set1, lego_set2, lego_set3]
-pp lego_sets
+lego_sets.each { |set|
+  pp set.design
+  pp set.price
+  pp set.difficulty
+}
+puts lego_set1
+lego_set1.price += 10
+puts lego_set1
