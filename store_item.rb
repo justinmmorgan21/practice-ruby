80a844b37989029c4d59bc9ed5a5bfd0ed70c998
justinmmorgan21 <justinmorgan21@gmail.com>
2024-08-27 07:51:05 -0500

    Use normalcase for identifier numbers

--- store_item.rb
+++ store_item.rb
@@ -1,20 +1,20 @@
-lego_set_1 = {
+lego_set1 = {
   design: "truck",
   price: 30,
   difficulty: "medium"
 }
 
-lego_set_2 = {
+lego_set2 = {
   design: "skyscraper",
   price: 50,
   difficulty: "hard"
 }
 
-lego_set_3 = {
+lego_set3 = {
   :design => "jet ski",
   :price => 20,
   :difficulty => "easy"
 }
 
-lego_sets = [lego_set_1, lego_set_2, lego_set_3]
+lego_sets = [lego_set1, lego_set2, lego_set3]
 pp lego_sets
