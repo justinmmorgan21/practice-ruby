3fa205abfa6449e1b7411549deaaaa315737a109
justinmmorgan21 <justinmorgan21@gmail.com>
2024-08-26 20:59:30 -0500

    Define lego sets as hashes

---
+++ store_item.rb
@@ -0,0 +1,20 @@
+lego_set_1 = {
+  design: "truck",
+  price: 30,
+  difficulty: "medium"
+}
+
+lego_set_2 = {
+  design: "skyscraper",
+  price: 50,
+  difficulty: "hard"
+}
+
+lego_set_3 = {
+  :design => "jet ski",
+  :price => 20,
+  :difficulty => "easy"
+}
+
+lego_sets = [lego_set_1, lego_set_2, lego_set_3]
+pp lego_sets
