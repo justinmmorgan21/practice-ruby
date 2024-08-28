eabe9ef4e25e6805ded81bd5e9208d9db31eab0d
justinmmorgan21 <justinmorgan21@gmail.com>
2024-08-28 08:03:38 -0500

    Filter all QB, WR, RB, TE, K, for each team

--- display_json.rb
+++ display_json.rb
@@ -8,46 +8,49 @@ require 'uri'
 require 'net/http'
 require 'json'
 
-url = URI("https://tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com/getNFLTeamRoster?teamID=6&teamAbv=CHI&getStats=true&fantasyPoints=true")
+def team_info(team_name)
+  url = URI("https://tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com/getNFLTeamRoster?teamID=6&teamAbv=" + team_name + "&getStats=true&fantasyPoints=true")
 
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
 
-request = Net::HTTP::Get.new(url)
-request["x-rapidapi-key"] = 'ENV['RAPID_API_KEY']'
-request["x-rapidapi-host"] = 'tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com'
+  request = Net::HTTP::Get.new(url)
+  request["x-rapidapi-key"] = 'ENV['RAPID_API_KEY']'
+  request["x-rapidapi-host"] = 'tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com'
 
-response = http.request(request)
+  response = http.request(request)
 
-puts response.read_body.class
 
-# json_string = '{"name": "GFG", "age": 30, "city": "Noida"}'
-json_object = JSON.parse(response.read_body)
-puts json_object["body"]["team"]
-roster = json_object["body"]["roster"]
-roster.each { |player|
-  puts player["longName"]
+  json_object = JSON.parse(response.read_body)
+  puts json_object["body"]["team"]
+  roster = json_object["body"]["roster"]
+  positions = {
+    QB: [],
+    WR: [],
+    RB: [],
+    TE: [],
+    K: []
+  }
+  roster.each { |player|
+    case player["pos"]
+    when "QB"
+      positions[:QB] << player["longName"]
+    when "WR"
+      positions[:WR] << player["longName"]
+    when "RB"
+      positions[:RB] << player["longName"]
+    when "TE"
+      positions[:TE] << player["longName"]
+    when "PK"
+      positions[:K] << player["longName"]
+    end
+  }
+  pp positions
+  puts
+end
+
+teams = ["BUF", "MIA", "NE", "NYJ", "DAL", "NYG", "PHI", "WAS", "BAL", "CIN", "CLE", "PIT", "CHI", "DET", "GB", "MIN", "HOU", "IND", "JAX", "TEN", "ATL", "CAR", "NO", "TB", "DEN", "KC", "LV", "LAC", "ARI", "LAR", "SF", "SEA"]
+teams.each { |team|
+  team_info(team)
 }
 
-puts
-
-url = URI("https://tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com/getNFLTeamRoster?teamID=6&teamAbv=SEA&getStats=true&fantasyPoints=true")
-
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
-
-request = Net::HTTP::Get.new(url)
-request["x-rapidapi-key"] = 'ENV['RAPID_API_KEY']'
-request["x-rapidapi-host"] = 'tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com'
-
-response = http.request(request)
-
-puts response.read_body.class
-
-# json_string = '{"name": "GFG", "age": 30, "city": "Noida"}'
-json_object = JSON.parse(response.read_body)
-puts json_object["body"]["team"]
-roster = json_object["body"]["roster"]
-roster.each { |player|
-  puts player["longName"]
-}
