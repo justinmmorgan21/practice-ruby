db224daba2c2b68687baf0bb5eda31df9666b47d
justinmmorgan21 <justinmorgan21@gmail.com>
2024-08-28 20:54:02 -0500

    add am pm

--- weather.rb
+++ weather.rb
@@ -7,31 +7,47 @@
 
 require 'http'
 
-puts "Enter the city for weather info: "
+print "Enter the city for weather info: "
 city_name = gets.chomp
+while city_name != "q"
 
-city = HTTP.get("http://api.openweathermap.org/geo/1.0/direct?q=#{city_name}&limit=5&appid=#{ENV['OPEN_WEATHER_API_KEY']}").parse
-lat = city[0]["lat"]
-lon = city[0]["lon"]
+  city = HTTP.get("http://api.openweathermap.org/geo/1.0/direct?q=#{city_name}&limit=5&appid=#{ENV['OPEN_WEATHER_API_KEY']}").parse
+  lat = city[0]["lat"]
+  lon = city[0]["lon"]
+
+  print "Would you like temperature units in Celsius or Farenheit? "
+  units = gets.chomp
+  temp_units = (units == "Farenheit" || units == "farenheit" || units == "F" || units == "f") ? "imperial" : "metric"
+
+  weather = HTTP.get("https://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{lon}&appid=#{ENV['OPEN_WEATHER_API_KEY']}&units=#{temp_units}").parse
 
 
-weather = HTTP.get("https://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{lon}&appid=#{ENV['OPEN_WEATHER_API_KEY']}&units=imperial").parse
+  condition = weather["weather"][0]["description"]
+  temp = weather["main"]["temp"]
+  humidity = weather["main"]["humidity"]
+  wind = weather["wind"]["speed"].to_i
+  wind_degrees = weather["wind"]["deg"]
+  sunset = weather["sys"]["sunset"]
+  wind_direction = "east"
+  puts "Right now we have #{condition} and a temperature of #{temp}\u00b0 in #{city_name}."
 
+  wind_direction = "north" if wind_degrees > 45 && wind_degrees < 135
+  wind_direction = "west" if wind_degrees >= 135 && wind_degrees < 225
+  wind_direction = "south" if wind_degrees >= 225 && wind_degrees < 315
+  puts "The current humidity is #{humidity}% and there is a breeze of #{wind}mph out of the #{wind_direction}."
 
-condition = weather["weather"][0]["description"]
-temp = weather["main"]["temp"]
-humidity = weather["main"]["humidity"]
-wind = weather["wind"]["speed"].to_i
-wind_degrees = weather["wind"]["deg"]
-sunset = weather["sys"]["sunset"]
-wind_direction = "east"
-puts "Right now we have #{condition} and a temperature of #{temp}\u00b0 in Frisco, Texas."
+  time = Time.at(sunset).to_s
+  sunset_clock_time = time[11,5]
+  hour = sunset_clock_time[0,2].to_i
+  if hour < 12
+    puts "#{sunset_clock_time}am"
+  else
+    hour -= 12
+    puts "#{sunset_clock_time}pm"
+  end
+  puts "Sunset tonight is at #{sunset_clock_time}"
 
-wind_direction = "north" if wind_degrees > 45 && wind_degrees < 135
-wind_direction = "west" if wind_degrees >= 135 && wind_degrees < 225
-wind_direction = "south" if wind_degrees >= 225 && wind_degrees < 315
-puts "The current humidity is #{humidity}% and there is a breeze of #{wind}mph out of the #{wind_direction}."
-
-puts Time.at(sunset)
-sunset_clock_time = "8:00pm"
-puts "Sunset tonight is at #{sunset_clock_time}"
+  print "Enter another city to find info for (q to quit): "
+  city_name = gets.chomp
+  
+end
